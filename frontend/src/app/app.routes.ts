b86e61229f5b98d5b1c7222a3853babe0a1b5aed
laiag <laiag.jrl@gmail.com>
2025-10-23 11:54:56 +0200

    minor changes in shell component, created draft version of contact-us-page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { TripsPageComponent } from './pages/trips-page/trips-page.component';
 import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'trips' },
   { path: 'trips', component: TripsPageComponent, title: 'Trips' },
   { path: 'settings', component: SettingsPageComponent, title: 'Settings' },
+  { path: 'contact', component: ContactUsPageComponent, title: 'Contact Us' },
   { path: '**', component: NotFoundPageComponent, title: 'Not Found' },
 ];
